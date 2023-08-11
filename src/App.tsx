d17f8f46c4d6befa410e2c209162c2a3e8a260a4
kjh311 <kjh311@gmail.com>
2023-08-11 10:52:23 -0600

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import './App.css'
 import Button from './components/Button'
+import NavBar from './components/NavBar'
+// import {Nav, NavDropdown} from "react-bootstrap";
 
 function App() {
 
   return (
     <div className="App">
+      <NavBar />
       <Button/>
     </div>
   )
